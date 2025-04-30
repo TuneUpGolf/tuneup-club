9b71591f60fa22b8895c6f096ac347ebdd7d6386
Isha Dadhania <isha.dadhania@brainvire.com>
2025-04-30 10:44:22 +0530

    remove multi-select lessons dropdown from post module and select user type from signup

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -139,14 +139,6 @@
                         {!! Form::hidden('country_code', null, []) !!}
                         {!! Form::hidden('dial_code', null, []) !!}
                     </div>
-                     <div class="form-group">
-                        {{ Form::label('type', __('Type'), ['class' => 'form-label']) }}
-                        {!! Form::select('type', ['Follower' => 'Follower', 'Influencer' => 'Influencer'], null, [
-                            'class' => 'form-control-new',
-                            'data-trigger',
-                            'placeholder' => __('User Type'),
-                        ]) !!}
-                    </div> 
 
                     <div class="form-check check-box">
                         <label class="switch">
