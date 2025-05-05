0320636437deaafeb12267ec9b0a512977577a4d
Isha Dadhania <isha.dadhania@brainvire.com>
2025-05-05 14:13:25 +0530

    add purchase detail page changes

--- resources/views/admin/purchases/purchaseVideoAction.blade.php
+++ resources/views/admin/purchases/purchaseVideoAction.blade.php
@@ -5,14 +5,14 @@
 </a>
 @if (!$purchaseVideo->feedback)
     <a class="btn btn-sm small btn btn-warning "
-        href="{{ route('purchase.feedback.create', ['purchase_video' => $purchaseVideo]) }}" data-bs-toggle="tooltip"
+        href="{{ route('purchase.feedback.create', ['purchase_video' => $purchaseVideo->video_url]) }}" data-bs-toggle="tooltip"
         data-bs-placement="bottom" data-bs-original-title="{{ __('Add Feedback') }}">
         <i class="ti ti-plus text-white"></i>
     </a>
 @endif
 @if (!!$purchaseVideo->feedback)
     <a class="btn btn-sm small btn btn-warning "
-        href="{{ route('purchase.feedback.create', ['purchase_video' => $purchaseVideo]) }}" data-bs-toggle="tooltip"
+        href="{{ route('purchase.feedback.create', ['purchase_video' => $purchaseVideo->video_url]) }}" data-bs-toggle="tooltip"
         data-bs-placement="bottom" data-bs-original-title="{{ __('Edit Feedback') }}">
         <i class="ti ti-plus text-white"></i>
     </a>
