f5d947359e7406e6b0ce53f5063ac86e0e7c0efa
zohaibahmed <zohaibahmed.dev@gmail.com>
2025-09-18 16:02:18 +0500

    hide link and unlink in view

--- resources/views/admin/lessons/create.blade.php
+++ resources/views/admin/lessons/create.blade.php
@@ -123,7 +123,8 @@
         CKEDITOR.replace('lesson_description', {
             allowedContent: true,
             filebrowserUploadUrl: "{{ route('ckeditor.upload', ['_token' => csrf_token()]) }}",
-            filebrowserUploadMethod: 'form'
+            filebrowserUploadMethod: 'form',
+            removeButtons: 'Link,Unlink'
         });
 
         CKEDITOR.replace('short_description', {
