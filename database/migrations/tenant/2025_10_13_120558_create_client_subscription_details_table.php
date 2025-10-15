5d79a2f9eaf53cafd949038208c92fe896b5be67
Omer Farooq <i.omer6637@gmail.com>
2025-10-15 10:55:41 +0500

    stripe monthly subscription plan setup + webhook

---
+++ database/migrations/tenant/2025_10_13_120558_create_client_subscription_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('client_subscription_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('client_subscription_id');
+            $table->unsignedInteger('invoice_id');
+            $table->unsignedInteger('payment_intent_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('client_subscription_details');
+    }
+};
