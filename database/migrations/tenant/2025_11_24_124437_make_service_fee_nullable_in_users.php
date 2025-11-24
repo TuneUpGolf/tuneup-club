8c3b6c29b087210d81ff3b94599935ac0a48fc9b
Omer Farooq <i.omer6637@gmail.com>
2025-11-24 17:45:51 +0500

    make service fee nullable

---
+++ database/migrations/tenant/2025_11_24_124437_make_service_fee_nullable_in_users.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            DB::statement('ALTER TABLE your_table_name MODIFY service_fee DECIMAL(10,2) NULL');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
