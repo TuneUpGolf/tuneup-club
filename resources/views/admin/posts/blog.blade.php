b3f36a8876b584fb03d1de6794574af5070b541d
Omer Farooq <i.omer6637@gmail.com>
2025-12-02 16:04:53 +0500

    test

--- resources/views/admin/posts/blog.blade.php
+++ resources/views/admin/posts/blog.blade.php
@@ -109,7 +109,7 @@ body.modal-open .dash-container {
         @if ($post->paid && !$purchasePost && !$isInfluencer && !$isSubscribed)
         <div class="relative paid-post-wrap">
                 <video class="w-full post-thumbnail pointer-events-none opacity-50" controls playsinline webkit-playsinline>
-                    <source src="{{ $post->file }}" type="video/mp4">
+                    <source src="{{ $post->file }}#t=0.001">
                 </video>
                 <div class="absolute inset-0 flex justify-center items-center paid-post flex-col">
                 <div
@@ -132,7 +132,7 @@ body.modal-open .dash-container {
         </div>
         @else
         <video controls class="w-full post-thumbnail" controls playsinline webkit-playsinline>
-            <source src="{{ $post->file }}" type="video/mp4">
+            <source src="{{ $post->file }}#t=0.001" >
         </video>
         @endif
         @endif
