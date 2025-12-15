55a7ec03cc050f5f200d88f5e0101712613f2a5a
Omer Farooq <i.omer6637@gmail.com>
2025-12-15 16:12:23 +0500

    test chat 2

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -66,6 +66,7 @@ class HomeController extends Controller
                 // Count user's messages
                 $userMessageCount = 0;
                 foreach ($messages as $message) {
+                    dd($message);
                     if (
                         isset($message['userData']['userId']) &&
                         $message['userData']['userId'] == $user->id
