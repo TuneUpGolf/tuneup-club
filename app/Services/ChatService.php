aba87edc8739875bc727b6f036c610f11482511e
Omer Farooq <i.omer6637@gmail.com>
2025-12-19 17:37:46 +0500

    test

--- app/Services/ChatService.php
+++ app/Services/ChatService.php
@@ -30,7 +30,7 @@ class ChatService
                 $user['chat_user_id'] = $chatUserId;
                 $user->save();
             }
-            // dd($user);
+            dd($user, $responseData);
             return true;
         } else {
             Log::error('Failed to create chat user', [
